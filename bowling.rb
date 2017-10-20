6c94dc7c4e49eace70fcd4a0833e98b57306f70a
Derek Morr <derekmorr@psu.edu>
2017-10-20 14:34:52 -0400

    refactor

--- bowling.rb
+++ bowling.rb
@@ -12,30 +12,26 @@ class Bowling
     # partial_totals = frames.map(&:sum) # makes a new array of numbers
     # partial_totals.sum # sums the numbers
 
-    frames.inject(Frame.new(0, false)) do |accumulator, throws|
-      this_frame = Score.new(throws).process_frame
+    frames.inject(State.new(0, false)) do |state, throws|
+      this_frame = Frame.new(throws)
       # if previous frame was a spare, add extra points
-      if accumulator.spare
-        new_score = 10 + throws[0] + this_frame.score + accumulator.tally
-        Frame.new(new_score, this_frame.spare)
+      if state.spare
+        new_score = 10 + throws[0] + this_frame.score + state.score
+        State.new(new_score, this_frame.spare?)
       else
-        new_score = this_frame.score + accumulator.tally
-        Frame.new(new_score, this_frame.spare)
+        new_score = this_frame.score + state.score
+        State.new(new_score, this_frame.spare?)
       end
-    end.tally
+    end.score
   end
 
 end
 
-class Score
+class Frame
   attr_reader :throws
 
   def initialize(throws)
     @throws = throws
-  end
-
-  def process_frame
-    Frame.new(tally, spare?)
   end
 
   def tally
@@ -45,22 +41,22 @@ class Score
   def spare?
     tally == 10
   end
-end
-
-class Frame
-  attr_reader :tally, :spare
-
-  def initialize(tally, spare)
-    @tally = tally
-    @spare = spare
-  end
 
   def score
     # if we got a spare, we collect the points on the next throw
-    if spare
+    if spare?
       0
     else
       tally
     end
+  end
+end
+
+class State
+  attr_reader :score, :spare
+
+  def initialize(score, spare)
+    @score = score
+    @spare = spare
   end
 end
